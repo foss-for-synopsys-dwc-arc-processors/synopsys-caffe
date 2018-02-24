40ffe8a8690581064e7e8d7542e977a885e27b96
Yvonne Chen <yche@synopsys.com>
2018-02-23 23:22:30 -0500

    Refine the Faster-RCNN patch

--- include/caffe/layers/fast_rcnn_layers.hpp
+++ include/caffe/layers/fast_rcnn_layers.hpp
@@ -13,7 +13,6 @@
 #include "caffe/blob.hpp"
 #include "caffe/common.hpp"
 #include "caffe/layer.hpp"
-#include "caffe/loss_layers.hpp"
 #include "caffe/proto/caffe.pb.h"
 
 namespace caffe {
@@ -54,48 +53,6 @@ class ROIPoolingLayer : public Layer<Dtype> {
   int pooled_width_;
   Dtype spatial_scale_;
   Blob<int> max_idx_;
-};
-
-template <typename Dtype>
-class SmoothL1LossLayer : public LossLayer<Dtype> {
- public:
-  explicit SmoothL1LossLayer(const LayerParameter& param)
-      : LossLayer<Dtype>(param), diff_() {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual inline const char* type() const { return "SmoothL1Loss"; }
-
-  virtual inline int ExactNumBottomBlobs() const { return -1; }
-  virtual inline int MinBottomBlobs() const { return 2; }
-  virtual inline int MaxBottomBlobs() const { return 4; }
-
-  /**
-   * Unlike most loss layers, in the SmoothL1LossLayer we can backpropagate
-   * to both inputs -- override to return true and always allow force_backward.
-   */
-  virtual inline bool AllowForceBackward(const int bottom_index) const {
-    return true;
-  }
-
- protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-
-  Blob<Dtype> diff_;
-  Blob<Dtype> errors_;
-  Blob<Dtype> ones_;
-  bool has_weights_;
-  Dtype sigma2_;
 };
 
 }  // namespace caffe
