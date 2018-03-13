57aad88ab9bb80c7c4f564be0638f6a431b3660b
Yvonne Chen <yche@synopsys.com>
2018-03-12 22:26:15 -0400

    Add the backwards support for Relu6

--- src/caffe/layers/relu_layer.cpp
+++ src/caffe/layers/relu_layer.cpp
@@ -31,9 +31,12 @@ void ReLULayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
     const int count = bottom[0]->count();
     Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
+    Dtype relu6 = this->layer_param_.relu_param().relu6(); //CUSTOMIZATION
     for (int i = 0; i < count; ++i) {
       bottom_diff[i] = top_diff[i] * ((bottom_data[i] > 0)
           + negative_slope * (bottom_data[i] <= 0));
+      if(relu6) //CUSTOMIZATION
+        bottom_diff[i] *= (bottom_data[i] < Dtype(6));
     }
   }
 }
