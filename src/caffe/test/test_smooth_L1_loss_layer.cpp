9392bf06dbe7cf9572defea8d3291c04028ddc6f
Yvonne Chen <yche@synopsys.com>
2018-03-23 02:47:37 -0400

    test_smooth_L1_loss_layer: Implement the separate tests for Faster RCNN and SSD.

--- src/caffe/test/test_smooth_L1_loss_layer.cpp
+++ src/caffe/test/test_smooth_L1_loss_layer.cpp
@@ -32,8 +32,10 @@ class SmoothL1LossLayerTest : public MultiDeviceTest<TypeParam> {
     GaussianFiller<Dtype> filler(filler_param);
     filler.Fill(this->blob_bottom_data_);
     blob_bottom_vec_.push_back(blob_bottom_data_);
+    blob_bottom_vec_SSD_.push_back(blob_bottom_data_);
     filler.Fill(this->blob_bottom_label_);
     blob_bottom_vec_.push_back(blob_bottom_label_);
+    blob_bottom_vec_SSD_.push_back(blob_bottom_label_);
     //const_filler.Fill(this->blob_bottom_inside_weights_); //Faster RCNN
     filler.Fill(this->blob_bottom_inside_weights_);
     blob_bottom_vec_.push_back(blob_bottom_inside_weights_);
@@ -41,6 +43,7 @@ class SmoothL1LossLayerTest : public MultiDeviceTest<TypeParam> {
     filler.Fill(this->blob_bottom_outside_weights_);
     blob_bottom_vec_.push_back(blob_bottom_outside_weights_);
     blob_top_vec_.push_back(blob_top_loss_);
+    blob_top_vec_SSD_.push_back(blob_top_loss_);
   }
   virtual ~SmoothL1LossLayerTest() {
     delete blob_bottom_data_;
@@ -50,26 +53,39 @@ class SmoothL1LossLayerTest : public MultiDeviceTest<TypeParam> {
     delete blob_top_loss_;
   }
 
-  void TestForward() {
+  void TestForward(bool SSD) {
     // Get the loss without a specified objective weight -- should be
     // equivalent to explicitly specifiying a weight of 1.
     LayerParameter layer_param;
-    SmoothL1LossParameter* loss_param = //Faster RCNN
-        layer_param.mutable_smooth_l1_loss_param();
-    loss_param->set_sigma(2.4); //Faster RCNN
+    if (!SSD) { //Faster RCNN
+      SmoothL1LossParameter* loss_param = layer_param.mutable_smooth_l1_loss_param();
+      loss_param->set_sigma(2.4); //Faster RCNN
+      loss_param->set_abssum(false); //Faster RCNN
+    }
     SmoothL1LossLayer<Dtype> layer_weight_1(layer_param);
-    layer_weight_1.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
-    const Dtype loss_weight_1 =
-        layer_weight_1.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
-
+    Dtype loss_weight_1;
+    if (SSD) {
+      layer_weight_1.SetUp(this->blob_bottom_vec_SSD_, this->blob_top_vec_SSD_);
+      loss_weight_1 = layer_weight_1.Forward(this->blob_bottom_vec_SSD_, this->blob_top_vec_SSD_);
+    }
+    else {
+      layer_weight_1.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+      loss_weight_1 = layer_weight_1.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+    }
     // Get the loss again with a different objective weight; check that it is
     // scaled appropriately.
     const Dtype kLossWeight = 3.7;
     layer_param.add_loss_weight(kLossWeight);
     SmoothL1LossLayer<Dtype> layer_weight_2(layer_param);
-    layer_weight_2.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
-    const Dtype loss_weight_2 =
-        layer_weight_2.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+    Dtype loss_weight_2;
+    if (SSD) {
+      layer_weight_2.SetUp(this->blob_bottom_vec_SSD_, this->blob_top_vec_SSD_);
+      loss_weight_2 = layer_weight_2.Forward(this->blob_bottom_vec_SSD_, this->blob_top_vec_SSD_);
+    }
+    else {
+      layer_weight_2.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+      loss_weight_2 = layer_weight_2.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+    }
     const Dtype kErrorMargin = 1e-5;
     EXPECT_NEAR(loss_weight_1 * kLossWeight, loss_weight_2, kErrorMargin);
     // Make sure the loss is non-trivial.
@@ -84,20 +100,39 @@ class SmoothL1LossLayerTest : public MultiDeviceTest<TypeParam> {
   Blob<Dtype>* const blob_top_loss_;
   vector<Blob<Dtype>*> blob_bottom_vec_;
   vector<Blob<Dtype>*> blob_top_vec_;
+  vector<Blob<Dtype>*> blob_bottom_vec_SSD_; //SSD
+  vector<Blob<Dtype>*> blob_top_vec_SSD_; //SSD
 };
 
 TYPED_TEST_CASE(SmoothL1LossLayerTest, TestDtypesAndDevices);
 
-TYPED_TEST(SmoothL1LossLayerTest, TestForward) {
-  this->TestForward();
+TYPED_TEST(SmoothL1LossLayerTest, TestForward_SSD) {
+  this->TestForward(true);
 }
 
-TYPED_TEST(SmoothL1LossLayerTest, TestGradient) {
+TYPED_TEST(SmoothL1LossLayerTest, TestForward_FCN) { //Faster RCNN
+  this->TestForward(false);
+}
+
+TYPED_TEST(SmoothL1LossLayerTest, TestGradient_SSD) {
+  typedef typename TypeParam::Dtype Dtype;
+  LayerParameter layer_param;
+  const Dtype kLossWeight = 3.7;
+  layer_param.add_loss_weight(kLossWeight);
+  SmoothL1LossLayer<Dtype> layer(layer_param);
+  layer.SetUp(this->blob_bottom_vec_SSD_, this->blob_top_vec_SSD_);
+  GradientChecker<Dtype> checker(1e-2, 1e-2, 1701);
+  checker.CheckGradientExhaustive(&layer, this->blob_bottom_vec_SSD_,
+      this->blob_top_vec_SSD_);
+}
+
+TYPED_TEST(SmoothL1LossLayerTest, TestGradient_FCN) { //Faster RCNN
   typedef typename TypeParam::Dtype Dtype;
   LayerParameter layer_param;
   SmoothL1LossParameter* loss_param = //Faster RCNN
       layer_param.mutable_smooth_l1_loss_param();
   loss_param->set_sigma(2.4); //Faster RCNN
+  loss_param->set_abssum(false); //Faster RCNN
   const Dtype kLossWeight = 3.7;
   layer_param.add_loss_weight(kLossWeight);
   SmoothL1LossLayer<Dtype> layer(layer_param);
