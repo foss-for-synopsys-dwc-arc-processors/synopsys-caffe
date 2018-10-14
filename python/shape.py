205470e1786e8cc99bf3edc5a626289aadcfbb18
Haifeng Han <haifeng@synopsys.com>
2018-10-14 17:25:42 +0800

    reformat shape.py

--- python/shape.py
+++ python/shape.py
@@ -12,24 +12,19 @@ class TensorShape(caffe.Layer):
         if len(bottom) != 1:
             raise Exception("Only input one Tensor at a time!")
         if len(top) != 1:
-	    raise Exception("Only output one Tensor at a time!")
-	
-	# other parameters...
-
+            raise Exception("Only output one Tensor at a time!")
 
     def reshape(self, bottom, top):
         # check input dimensions
         if bottom[0].count == 0:
             raise Exception("Input must not be empty!")
         top[0].reshape(len(bottom[0].data.shape))
-		
 
     def forward(self, bottom, top):
-	top[0].data[...] = bottom[0].data.shape
-
+        top[0].data[...] = bottom[0].data.shape
 
     def backward(self, top, propagate_down, bottom):
-	for i in range(len(propagate_down)):
+        for i in range(len(propagate_down)):
             if not propagate_down[i]:
                 continue
             bottom[i].diff[...] = top[i].diff[:]
