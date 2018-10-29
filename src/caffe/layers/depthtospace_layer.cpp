c1728e2834c928acefac655b281433d240924f80
Haifeng Han <haifeng@synopsys.com>
2018-10-29 17:04:19 +0800

    implementation of tf.depth_to_space in NHWC data format only

---
+++ src/caffe/layers/depthtospace_layer.cpp
@@ -0,0 +1,78 @@
+#include <vector>
+
+#include "caffe/layers/depthtospace_layer.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void DepthToSpaceLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  CHECK_NE(top[0], bottom[0]) << this->type() << " Layer does not "
+      "allow in-place computation.";
+  this->block_size = this->layer_param_.depth_to_space_param().block_size();
+  this->data_format = this->layer_param_.depth_to_space_param().data_format();
+  vector<int> bottom_shape = bottom[0]->shape();
+  this->output_top_shape.push_back(bottom_shape[0]);
+  if(this->data_format == "NHWC"){
+    this->output_top_shape.push_back(bottom_shape[1] * this->block_size);
+    this->output_top_shape.push_back(bottom_shape[2] * this->block_size);
+    this->output_top_shape.push_back(bottom_shape[3] / (this->block_size*this->block_size));
+  } else if(this->data_format == "NCHW"){
+    NOT_IMPLEMENTED;
+    // this->output_top_shape.push_back(bottom_shape[1] / (this->block_size*this->block_size));
+    // this->output_top_shape.push_back(bottom_shape[2] * this->block_size);
+    // this->output_top_shape.push_back(bottom_shape[3] * this->block_size);
+  }
+}
+
+template <typename Dtype>
+void DepthToSpaceLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  top[0]->Reshape(this->output_top_shape);
+  CHECK_EQ(top[0]->count(), bottom[0]->count())
+    << "output count must match input count";
+}
+
+template <typename Dtype>
+void DepthToSpaceLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top){
+  if (this->data_format == "NHWC"){
+    const int batch_size = this->output_top_shape[0];
+    const int output_height = this->output_top_shape[1];
+    const int output_width = this->output_top_shape[2];
+    const int output_depth = this->output_top_shape[3];
+
+    vector<int> bottom_shape = bottom[0]->shape();
+    const int input_height = bottom_shape[1];
+    const int input_width = bottom_shape[2];
+    const int input_depth = bottom_shape[3];
+
+    const Dtype* bottom_data = bottom[0]->cpu_data();
+    Dtype* top_data = top[0]->mutable_cpu_data();
+
+    for (int b = 0; b < batch_size; ++b) {
+      for (int h = 0; h < output_height; ++h) {
+        const int in_h = h / this->block_size;
+        const int offset_h = (h % this->block_size);
+        for (int w = 0; w < output_width; ++w) {
+          const int in_w = w / this->block_size;
+          const int offset_w = (w % this->block_size);
+          const int offset_d =
+            (offset_h * this->block_size + offset_w) * output_depth;
+          for (int d = 0; d < output_depth; ++d) {
+            const int in_d = d + offset_d;
+            const int out_index = b*output_height*output_width*output_depth + h*output_width*output_depth + w*output_depth + d;
+            const int in_index = b*input_height*input_width*input_depth + in_h*input_width*input_depth + in_w*input_depth + in_d;
+            top_data[out_index] = bottom_data[in_index];
+          }
+        }
+      }
+    }
+  }
+}
+
+
+INSTANTIATE_CLASS(DepthToSpaceLayer);
+REGISTER_LAYER_CLASS(DepthToSpace);
+
+}  // namespace caffe
