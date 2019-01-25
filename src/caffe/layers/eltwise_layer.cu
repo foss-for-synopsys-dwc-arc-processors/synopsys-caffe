987b2318937d3e1285675d95e795f6e31e46b023
Yvonne Chen <yche@synopsys.com>
2019-01-25 09:21:40 +0100

    add CUDA implementation for MIN op in eltwise (no broadcasting yet)

--- src/caffe/layers/eltwise_layer.cu
+++ src/caffe/layers/eltwise_layer.cu
@@ -30,6 +30,32 @@ __global__ void MaxForward(const int nthreads, const Dtype* bottom_data_a,
   }
 }
 
+//<--CUSTOMIZATION
+template <typename Dtype>
+__global__ void MinForward(const int nthreads, const Dtype* bottom_data_a,
+    const Dtype* bottom_data_b, const int blob_idx, Dtype* top_data,
+    int* mask) {
+  CUDA_KERNEL_LOOP(index, nthreads) {
+    Dtype minval = FLT_MAX;
+    int minidx = -1;
+    if (bottom_data_a[index] < bottom_data_b[index]) {
+      // only update for very first bottom_data blob (blob_idx == 0)
+      if (blob_idx == 0) {
+        minval = bottom_data_a[index];
+        top_data[index] = minval;
+        minidx = blob_idx;
+        mask[index] = minidx;
+      }
+    } else {
+      minval = bottom_data_b[index];
+      top_data[index] = minval;
+      minidx = blob_idx + 1;
+      mask[index] = minidx;
+    }
+  }
+}
+//CUSTOMIZATION-->
+
 template <typename Dtype>
 void EltwiseLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
@@ -79,6 +105,19 @@ void EltwiseLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
           count, top_data, bottom[i]->gpu_data(), i-1, top_data, mask);
     }
     break;
+  //<--CUSTOMIZATION
+  case EltwiseParameter_EltwiseOp_MIN:
+    mask = min_idx_.mutable_gpu_data();
+    // NOLINT_NEXT_LINE(whitespace/operators)
+    MinForward<Dtype> <<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+        count, bottom[0]->gpu_data(), bottom[1]->gpu_data(), 0, top_data, mask);
+    for (int i = 2; i < bottom.size(); ++i) {
+      // NOLINT_NEXT_LINE(whitespace/operators)
+      MinForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+          count, top_data, bottom[i]->gpu_data(), i-1, top_data, mask);
+    }
+    break;
+    //CUSTOMIZATION-->
   default:
     LOG(FATAL) << "Unknown elementwise operation.";
   }
@@ -95,6 +134,20 @@ __global__ void MaxBackward(const int nthreads, const Dtype* top_diff,
     bottom_diff[index] = gradient;
   }
 }
+
+//<--CUSTOMIZATION
+template <typename Dtype>
+__global__ void MinBackward(const int nthreads, const Dtype* top_diff,
+    const int blob_idx, const int* mask, Dtype* bottom_diff) {
+  CUDA_KERNEL_LOOP(index, nthreads) {
+    Dtype gradient = 0;
+    if (mask[index] == blob_idx) {
+      gradient += top_diff[index];
+    }
+    bottom_diff[index] = gradient;
+  }
+}
+//CUSTOMIZATION-->
 
 template <typename Dtype>
 void EltwiseLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
@@ -142,6 +195,14 @@ void EltwiseLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
             <<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
             count, top_diff, i, mask, bottom_diff);
         break;
+      //<--CUSTOMIZATION
+      case EltwiseParameter_EltwiseOp_MIN:
+        mask = min_idx_.gpu_data();
+        MinBackward<Dtype>  // NOLINT_NEXT_LINE(whitespace/operators)
+            <<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+            count, top_diff, i, mask, bottom_diff);
+        break;
+        //CUSTOMIZATION-->
       default:
         LOG(FATAL) << "Unknown elementwise operation.";
       }
