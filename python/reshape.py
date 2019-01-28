e1f0092201dcb9570e86403f06d6fe281675b43f
Haifeng Han <haifenghan@qq.com>
2019-01-28 15:54:24 +0800

    reshape operation for the second input is not a constant

---
+++ python/reshape.py
@@ -0,0 +1,32 @@
+import caffe
+import numpy as np
+
+
+class Reshape(caffe.Layer):
+    """
+    implementation of the tf.reshape() with two inputs.
+    """
+    def setup(self, bottom, top):
+        # check number of inputs and outputs
+        # bottom[1] is blob to be reshaped
+        # bottom[2] is shape
+        if len(bottom) != 2:
+            raise Exception("Please input two Tensors!")
+        if len(top) != 1:
+            raise Exception("Only output one Tensor at a time!")
+
+    def reshape(self, bottom, top):
+        # check input dimensions
+        if bottom[0].count == 0:
+            raise Exception("Input must not be empty!")
+        shape = [int(i) for i in bottom[1].data]
+        top[0].reshape(*shape)
+
+    def forward(self, bottom, top):
+        top[0].data[...] = bottom[0].data
+
+    def backward(self, top, propagate_down, bottom):
+        for i in range(len(propagate_down)):
+            if not propagate_down[i]:
+                continue
+            bottom[i].diff[...] = top[i].diff[:]
