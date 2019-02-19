dd2dca8b952c2de62690dd8300a9e2ed6af6de60
Yvonne Chen <yche@synopsys.com>
2019-02-19 08:29:56 +0100

    fix the path assign for the windows platform call error.

--- src/caffe/layers/data_augmentation_layer.cpp
+++ src/caffe/layers/data_augmentation_layer.cpp
@@ -72,7 +72,9 @@ void DataAugmentationLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom
     this->layer_param_.mutable_param(this->layer_param_.param_size()-1)->set_decay_mult(0.); 
 //     LOG(INFO) << "DEBUG: this->layer_param_.param_size()=" << this->layer_param_.param_size();
 //     LOG(INFO) << "DEBUG: Writing layer_param";
+#if !defined(_MSC_VER)
     WriteProtoToTextFile(this->layer_param_, "/misc/lmbraid17/sceneflownet/dosovits/matlab/test/message.prototxt");
+#endif
 //     LOG(INFO) << "DEBUG: Finished writing layer_param";
   } 
 }
