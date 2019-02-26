59749ac1f14d3af3e2a300397631a03a9a906b6e
Minglu Li <minglu@synopsys.com>
2019-02-26 09:22:03 +0100

    add gather_layer.cpp and gather_layer.hpp

---
+++ include/caffe/layers/gather_layer.hpp
@@ -0,0 +1,59 @@
+#ifndef CAFFE_GATHER_LAYER_HPP_
+#define CAFFE_GATHER_LAYER_HPP_
+
+#include <vector>
+#include <string>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+
+
+namespace caffe {
+/*
+* @brief Resize images to size using nearest neighbor interpolation. ////
+* Note: implementation of tf.gather
+* https://www.tensorflow.org/api_docs/python/tf/gather
+*/
+
+template <typename Dtype>
+class GatherLayer : public Layer<Dtype> {
+public:
+ explicit GatherLayer(const LayerParameter& param)
+     : Layer<Dtype>(param) {}
+ virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+	 const vector<Blob<Dtype>*>& top);
+ virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+	 const vector<Blob<Dtype>*>& top);
+
+ virtual inline const char* type() const { return "Gather"; }
+ virtual inline int ExactNumBottomBlobs() const { return 1; }
+ virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+	  const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top, 
+	  const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+	  NOT_IMPLEMENTED;
+  }
+ //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+ //    const vector<Blob<Dtype>*>& top) {}
+ //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+ //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
+
+  int count_;
+  int num_gather_;      
+  int gather_size_;     
+  int gather_axis_;     
+  int indices_dim_;
+  vector<int> indices_;  
+  vector<int> indices_shape_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_GATHER_LAYER_HPP_
+
