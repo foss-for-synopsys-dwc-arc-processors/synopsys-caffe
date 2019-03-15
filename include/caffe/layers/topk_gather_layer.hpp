67e8d08b7e5d70bce007ab3f77b7a9b7a21f20ea
Yvonne Chen <yche@synopsys.com>
2019-03-15 10:33:58 +0100

    add the missing topk_gather header file

---
+++ include/caffe/layers/topk_gather_layer.hpp
@@ -0,0 +1,52 @@
+#ifndef CAFFE_TOPK_GATHER_LAYER_HPP_
+#define CAFFE_TOPK_GATHER_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+template <typename Dtype>
+class TopkGatherLayer : public Layer<Dtype> {
+ public:
+
+  explicit TopkGatherLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "TopkGather"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    NOT_IMPLEMENTED;
+  }
+
+  size_t top_k_;
+  bool has_axis_;
+  int axis_;
+
+  Blob<Dtype> topk_indices_;
+
+  int num_gather_;
+  int gather_size_;
+  int gather_axis_;
+  int indices_dim_;
+  vector<int> indices_shape_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_TOPK_GATHER_LAYER_HPP_
