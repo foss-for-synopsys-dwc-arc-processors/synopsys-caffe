1412e44476aa27589eb478f3610ca98ab119821f
Huaqi Fang <Huaqi.Fang@synopsys.com>
2019-05-26 21:55:06 -0400

    layer: Add xlu layer for dispatch
    Signed-off-by: Huaqi Fang <Huaqi.Fang@synopsys.com>

---
+++ include/caffe/layers/xlu_layer.hpp
@@ -0,0 +1,89 @@
+#ifndef CAFFE_XLU_LAYER_HPP_
+#define CAFFE_XLU_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Rectified Linear Unit non-linearity @f$ y = \max(0, x) @f$.
+ *        The simple max is fast to compute, and the function does not saturate.
+ */
+template <typename Dtype>
+class XLULayer : public Layer<Dtype> {
+ public:
+  /**
+   * @param param provides XLUParameter relu_param,
+   *     with XLULayer options:
+   *   - negative_slope (\b optional, default 0).
+   *     the value @f$ \nu @f$ by which negative values are multiplied.
+   */
+  explicit XLULayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+
+  virtual inline const char* type() const { return "XLU"; }
+  virtual inline int MinBottomBlobs() const { return 1; }
+  virtual inline int MaxBottomBlobs() const { return 2; } //CUSTOMIZATION
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+ protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the computed outputs @f$
+   *        y = \max(0, x)
+   *      @f$ by default.  If a non-zero negative_slope @f$ \nu @f$ is provided,
+   *      the computed outputs are @f$ y = \max(0, x) + \nu \min(0, x) @f$.
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  /**
+   * @brief Computes the error gradient w.r.t. the XLU inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x} = \left\{
+   *        \begin{array}{lr}
+   *            0 & \mathrm{if} \; x \le 0 \\
+   *            \frac{\partial E}{\partial y} & \mathrm{if} \; x > 0
+   *        \end{array} \right.
+   *      @f$ if propagate_down[0], by default.
+   *      If a non-zero negative_slope @f$ \nu @f$ is provided,
+   *      the computed gradients are @f$
+   *        \frac{\partial E}{\partial x} = \left\{
+   *        \begin{array}{lr}
+   *            \nu \frac{\partial E}{\partial y} & \mathrm{if} \; x \le 0 \\
+   *            \frac{\partial E}{\partial y} & \mathrm{if} \; x > 0
+   *        \end{array} \right.
+   *      @f$.
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_RELU_LAYER_HPP_
---
+++ src/caffe/layers/xlu_layer.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <vector>
+
+#include "caffe/layers/xlu_layer.hpp"
+
+namespace caffe {
+template <typename Dtype>
+void XLULayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+}
+
+template <typename Dtype>
+void XLULayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  top[0]->ReshapeLike(*bottom[0]);
+}
+
+template <typename Dtype>
+void XLULayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  Dtype* top_data = top[0]->mutable_cpu_data();
+  const int count = bottom[0]->count();
+  Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
+  Dtype relu6 = this->layer_param_.relu_param().relu6(); //CUSTOMIZATION
+  Dtype maximum = this->layer_param_.relu_param().maximum(); //CUSTOMIZATION
+  Dtype minimum = this->layer_param_.relu_param().minimum(); //CUSTOMIZATION
+  if (bottom.size() > 1)  //bottom[1] provides the maximum case
+  	maximum = bottom[1]->cpu_data()[0];
+  for (int i = 0; i < count; ++i) {
+    top_data[i] = std::max(bottom_data[i], Dtype(0))
+        + negative_slope * std::min(bottom_data[i], Dtype(0));
+    if(isnan(top_data[i])) //CUSTOMIZATION
+      top_data[i] = 0;
+    if(relu6) //CUSTOMIZATION
+      top_data[i] = std::min(top_data[i], Dtype(6)); //CUSTOMIZATION
+    if(maximum > Dtype(0))
+      top_data[i] = std::min(top_data[i], maximum); //CUSTOMIZATION
+    if(minimum != Dtype(0))
+      top_data[i] = std::max(top_data[i], minimum); //CUSTOMIZATION
+  }
+}
+
+template <typename Dtype>
+void XLULayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down,
+    const vector<Blob<Dtype>*>& bottom) {
+  if (propagate_down[0]) {
+    const Dtype* bottom_data = bottom[0]->cpu_data();
+    const Dtype* top_diff = top[0]->cpu_diff();
+    Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+    const int count = bottom[0]->count();
+    Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
+    Dtype relu6 = this->layer_param_.relu_param().relu6(); //CUSTOMIZATION
+    Dtype maximum = this->layer_param_.relu_param().maximum(); //CUSTOMIZATION
+    Dtype minimum = this->layer_param_.relu_param().minimum(); //CUSTOMIZATION
+    if (bottom.size() > 1)  //bottom[1] provides the maximum case
+      maximum = bottom[1]->cpu_data()[0];
+    for (int i = 0; i < count; ++i) {
+      bottom_diff[i] = top_diff[i] * ((bottom_data[i] > 0)
+          + negative_slope * (bottom_data[i] <= 0));
+      if(relu6) //CUSTOMIZATION
+        bottom_diff[i] *= (bottom_data[i] < Dtype(6));
+      if(maximum > Dtype(0)) //CUSTOMIZATION
+        bottom_diff[i] *= (bottom_data[i] < maximum);
+      if(minimum != Dtype(0)) //CUSTOMIZATION
+        bottom_diff[i] *= (bottom_data[i] > minimum);
+    }
+  }
+}
+
+
+#ifdef CPU_ONLY
+STUB_GPU(XLULayer);
+#endif
+
+INSTANTIATE_CLASS(XLULayer);
+REGISTER_LAYER_CLASS(XLU);
+
+}  // namespace caffe
