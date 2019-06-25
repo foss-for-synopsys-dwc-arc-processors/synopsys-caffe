aaa0406eeeeebd125bdac6f9e7d757c526d0c7a4
Yvonne Chen <yche@synopsys.com>
2019-06-25 11:00:57 +0200

    add the sub layer with broadcasting function

--- src/caffe/layers/sub_layer.cpp
+++ src/caffe/layers/sub_layer.cpp
@@ -3,20 +3,20 @@
 #include <utility>
 #include <vector>
 
-#include "caffe/layers/add_layer.hpp"
+#include "caffe/layers/sub_layer.hpp"
 
 namespace caffe {
 template <typename Dtype>
-void AddLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+void SubLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   CHECK_NE(top[0], bottom[0]) << this->type() << " Layer does not allow in-place computation.";
   is_scalar_=false;
-  Blob<Dtype>* addend = (bottom.size() > 1) ? bottom[1] : this->blobs_[0].get();
+  Blob<Dtype>* subtrahend = (bottom.size() > 1) ? bottom[1] : this->blobs_[0].get();
 
-  if(bottom[0]->num_axes()==0 || addend->num_axes()==0)
+  if(bottom[0]->num_axes()==0 || subtrahend->num_axes()==0)
     is_scalar_=true;
-  dim_diff_ = bottom[0]->num_axes() - addend->num_axes();
-  dim_ = bottom[0]->num_axes() >= addend->num_axes() ? bottom[0]->num_axes() : addend->num_axes();
+  dim_diff_ = bottom[0]->num_axes() - subtrahend->num_axes();
+  dim_ = bottom[0]->num_axes() >= subtrahend->num_axes() ? bottom[0]->num_axes() : subtrahend->num_axes();
   vector<int> top_shape(dim_, 1);
   if(dim_diff_ == 0)
   {
@@ -24,9 +24,9 @@ void AddLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
     {
       for(int i=0;i<dim_;i++)
       {
-        CHECK(bottom[0]->shape(i)==addend->shape(i) || bottom[0]->shape(i)==1 || addend->shape(i)==1)
+        CHECK(bottom[0]->shape(i)==subtrahend->shape(i) || bottom[0]->shape(i)==1 || subtrahend->shape(i)==1)
               << "Dimensions must be equal or 1 in the bottoms!";
-        top_shape[i] = bottom[0]->shape(i) >= addend->shape(i) ? bottom[0]->shape(i): addend->shape(i);
+        top_shape[i] = bottom[0]->shape(i) >= subtrahend->shape(i) ? bottom[0]->shape(i): subtrahend->shape(i);
       }
     }
   }
@@ -37,7 +37,7 @@ void AddLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       for(int i=0;i<dim_diff_;i++)
         top_shape[i] = bottom[0]->shape(i);
       for(int i=dim_diff_; i<dim_; i++)
-        top_shape[i] = bottom[0]->shape(i) >= addend->shape(i-dim_diff_) ? bottom[0]->shape(i): addend->shape(i-dim_diff_);
+        top_shape[i] = bottom[0]->shape(i) >= subtrahend->shape(i-dim_diff_) ? bottom[0]->shape(i): subtrahend->shape(i-dim_diff_);
     }
     else //bottom1 is a scalar
     {
@@ -50,14 +50,14 @@ void AddLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
     if(!is_scalar_)
     {
       for(int i=0;i<-dim_diff_;i++)
-        top_shape[i] = addend->shape(i);
+        top_shape[i] = subtrahend->shape(i);
       for(int i=-dim_diff_; i<dim_; i++)
-        top_shape[i] = bottom[0]->shape(i+dim_diff_) >= addend->shape(i) ? bottom[0]->shape(i+dim_diff_): addend->shape(i);
+        top_shape[i] = bottom[0]->shape(i+dim_diff_) >= subtrahend->shape(i) ? bottom[0]->shape(i+dim_diff_): subtrahend->shape(i);
     }
     else //bottom0 is a scalar
     {
       for(int i=0;i<dim_;i++)
-        top_shape[i] = addend->shape(i);
+        top_shape[i] = subtrahend->shape(i);
     }
   }
 
@@ -65,11 +65,11 @@ void AddLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 }
 
 template <typename Dtype>
-void AddLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+void SubLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
 	  const Dtype* bottom0_data = bottom[0]->cpu_data();
-	  Blob<Dtype>* addend = (bottom.size() > 1) ? bottom[1] : this->blobs_[0].get();
-	  const Dtype* bottom1_data = addend->cpu_data();
+	  Blob<Dtype>* subtrahend = (bottom.size() > 1) ? bottom[1] : this->blobs_[0].get();
+	  const Dtype* bottom1_data = subtrahend->cpu_data();
 	  Dtype* top_data = top[0]->mutable_cpu_data();
     int count = top[0]->count();
 
@@ -94,13 +94,13 @@ void AddLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
           {
             int num = (d % top[0]->count(i)) / top[0]->count(i+1);
             int n0 = 1 == bottom[0]->shape(i) ? 0 : num;
-            int n1 = 1 == addend->shape(i) ? 0 : num;
+            int n1 = 1 == subtrahend->shape(i) ? 0 : num;
             offset0 += n0 * bottom[0]->count(i+1);
-            offset1 += n1 * addend->count(i+1);
+            offset1 += n1 * subtrahend->count(i+1);
           }
           int z = d % top[0]->shape(dim_-1);
           int z0 = 1 == bottom[0]->shape(dim_-1) ? 0 : z;
-          int z1 = 1 == addend->shape(dim_-1) ? 0 : z;
+          int z1 = 1 == subtrahend->shape(dim_-1) ? 0 : z;
           offset0 += z0;
           offset1 += z1;
         }
@@ -116,13 +116,13 @@ void AddLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
           {
             int num = (d % top[0]->count(i)) / top[0]->count(i+1);
             int n0 = 1 == bottom[0]->shape(i) ? 0 : num;
-            int n1 = 1 == addend->shape(i-dim_diff_) ? 0 : num;
+            int n1 = 1 == subtrahend->shape(i-dim_diff_) ? 0 : num;
             offset0 += n0 * bottom[0]->count(i+1);
-            offset1 += n1 * addend->count(i-dim_diff_+1);
+            offset1 += n1 * subtrahend->count(i-dim_diff_+1);
           }
           int z = d % top[0]->shape(dim_-1);
           int z0 = 1 == bottom[0]->shape(dim_-1) ? 0 : z;
-          int z1 = 1 == addend->shape(dim_-dim_diff_-1) ? 0 : z;
+          int z1 = 1 == subtrahend->shape(dim_-dim_diff_-1) ? 0 : z;
           offset0 += z0;
           offset1 += z1;
         }
@@ -131,46 +131,46 @@ void AddLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
           for(int i=0;i<-dim_diff_;i++)
           {
             int num = (d % top[0]->count(i)) / top[0]->count(i+1);
-            int n1 = 1 == addend->shape(i) ? 0 : num;
-            offset1 += n1 * addend->count(i+1);
+            int n1 = 1 == subtrahend->shape(i) ? 0 : num;
+            offset1 += n1 * subtrahend->count(i+1);
           }
           for(int i=-dim_diff_;i<dim_-1;i++)
           {
             int num = (d % top[0]->count(i)) / top[0]->count(i+1);
             int n0 = 1 == bottom[0]->shape(i+dim_diff_) ? 0 : num;
-            int n1 = 1 == addend->shape(i) ? 0 : num;
+            int n1 = 1 == subtrahend->shape(i) ? 0 : num;
             offset0 += n0 * bottom[0]->count(i+dim_diff_+1);
-            offset1 += n1 * addend->count(i+1);
+            offset1 += n1 * subtrahend->count(i+1);
           }
           int z = d % top[0]->shape(dim_-1);
           int z0 = 1 == bottom[0]->shape(dim_+dim_diff_-1) ? 0 : z;
-          int z1 = 1 == addend->shape(dim_-1) ? 0 : z;
+          int z1 = 1 == subtrahend->shape(dim_-1) ? 0 : z;
           offset0 += z0;
           offset1 += z1;
         }
 
-        top_data[d] = bottom0_data[offset0] + bottom1_data[offset1];
+        top_data[d] = bottom0_data[offset0] - bottom1_data[offset1];
       }
     }
     else //is scalar with shape ()
     {
-      if(addend->num_axes()==0) //bottom1 is a scalar
+      if(subtrahend->num_axes()==0) //bottom1 is a scalar
       {
         caffe_copy(count, bottom0_data, top_data);
-        Dtype scalar = bottom1_data[0];
+        Dtype scalar = -bottom1_data[0]; //*-1 as sub
         caffe_add_scalar(count, scalar, top_data);
       }
       else //bottom0 is a scalar
       {
-        caffe_copy(count, bottom1_data, top_data);
+        caffe_cpu_scale(count, Dtype(-1), bottom1_data, top_data); //*-1 to be subtracted
         Dtype scalar = bottom0_data[0];
         caffe_add_scalar(count, scalar, top_data);
+
       }
     }
 }
 
-
-INSTANTIATE_CLASS(AddLayer);
-REGISTER_LAYER_CLASS(Add);
+INSTANTIATE_CLASS(SubLayer);
+REGISTER_LAYER_CLASS(Sub);
 
 }  // namespace caffe
