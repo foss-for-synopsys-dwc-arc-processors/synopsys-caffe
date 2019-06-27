0ae21a3a49dd1386a15127bb61d11edde70f26c8
Haifeng Han <haifenghan@qq.com>
2019-06-28 00:17:57 +0800

    optimize and reformat unstack layer

--- src/caffe/layers/unstack_layer.cpp
+++ src/caffe/layers/unstack_layer.cpp
@@ -7,70 +7,47 @@
 namespace caffe {
 
 template <typename Dtype>
-void UnstackLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-  const UnstackParameter& unstack_param = this->layer_param_.unstack_param();
+void UnstackLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype> *> &bottom,
+                                     const vector<Blob<Dtype> *> &top) {
+  const UnstackParameter &unstack_param = this->layer_param_.unstack_param();
   unstack_axis_ = bottom[0]->CanonicalAxisIndex(unstack_param.axis());
-  const int num = unstack_param.num();
-	if (num != 0) {
-		CHECK_EQ(num, bottom[0]->shape(unstack_axis_))
-			<< "num should equal to the shape in axis!";
-	}
-  unstack_num_ = bottom[0]->shape(unstack_axis_);
-  CHECK_EQ(unstack_num_, top.size())<< "Number of top blobs (" 
-			<< top.size() << ") should euqal to "<< "shape in axis (" 
-			<< unstack_num_ << ")";
+  unstack_num_ = unstack_param.num();
+  if (unstack_num_ == 0)
+    unstack_num_ = bottom[0]->shape(unstack_axis_);
 }
 
 template <typename Dtype>
-void UnstackLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-  const int num_axes = bottom[0]->num_axes();
-  //const UnstackParameter& unstack_param = this->layer_param_.unstack_param();
-	vector<int> bottom_shape = bottom[0]->shape();
+void UnstackLayer<Dtype>::Reshape(const vector<Blob<Dtype> *> &bottom,
+                                  const vector<Blob<Dtype> *> &top) {
+  // const UnstackParameter& unstack_param = this->layer_param_.unstack_param();
   vector<int> top_shape = bottom[0]->shape();
-	top_shape.resize(num_axes - 1);
-  num_unstack_ = bottom[0]->count(0, unstack_axis_);
-  unstack_size_ = bottom[0]->count(unstack_axis_ + 1);
-	int count = 0;
-	for (int i = unstack_axis_; i < num_axes - 1; ++i) {
-		top_shape[i] = bottom_shape[i + 1];
-	}
-  for (int i = 0; i < top.size(); ++i) {
-		top[i]->Reshape(top_shape);
-		count += top[i]->count();
-  }
-  CHECK_EQ(count, bottom[0]->count());
-  if (top.size() == 1) {
-    top[0]->ShareData(*bottom[0]);
-    top[0]->ShareDiff(*bottom[0]);
-  }
+  top_shape.erase(top_shape.begin() + unstack_axis_);
+  for (int i = 0; i < top.size(); ++i)
+    top[i]->Reshape(top_shape);
 }
 
 template <typename Dtype>
-void UnstackLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-  if (top.size() == 1) { return; }
-  int offset_unstack_axis = 0;
-  const Dtype* bottom_data = bottom[0]->cpu_data();
-  const int bottom_unstack_axis = bottom[0]->shape(unstack_axis_);
+void UnstackLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype> *> &bottom,
+                                      const vector<Blob<Dtype> *> &top) {
+  const Dtype *bottom_data = bottom[0]->cpu_data();
+  const int size_unstack_axis = bottom[0]->shape(unstack_axis_);
+  vector<int> bottom_shape = bottom[0]->shape();
+  int strides = 1;
+  for (int i = unstack_axis_ + 1; i < bottom_shape.size(); i++)
+    strides *= bottom_shape[i];
+  int num_unstack_ = bottom[0]->count() / strides / size_unstack_axis;
+  // num_unstack_ /= size_unstack_axis;
   for (int i = 0; i < top.size(); ++i) {
-    Dtype* top_data = top[i]->mutable_cpu_data();
+    Dtype *top_data = top[i]->mutable_cpu_data();
     for (int n = 0; n < num_unstack_; ++n) {
-      const int top_offset = n * unstack_size_;
-      const int bottom_offset =
-          (n * bottom_unstack_axis + offset_unstack_axis) * unstack_size_;
-      caffe_copy(unstack_size_,
-          bottom_data + bottom_offset, top_data + top_offset);
+      const int top_offset = n * strides;
+      const int bottom_offset = (n * size_unstack_axis + i) * strides;
+      caffe_copy(strides, bottom_data + bottom_offset, top_data + top_offset);
     }
-    offset_unstack_axis += 1;
   }
 }
-
-
 
 INSTANTIATE_CLASS(UnstackLayer);
 REGISTER_LAYER_CLASS(Unstack);
 
-}  // namespace caffe
-
+} // namespace caffe
