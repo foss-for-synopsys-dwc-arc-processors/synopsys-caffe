7247f1875c2e16df49ee9341c47b775e0657b914
Huaqi Fang <Huaqi.Fang@synopsys.com>
2019-08-14 08:04:15 +0200

    travis: try to use different cuda package, see whether it works
    Signed-off-by: Huaqi Fang <Huaqi.Fang@synopsys.com>

--- scripts/travis/install-deps.sh
+++ scripts/travis/install-deps.sh
@@ -90,7 +90,7 @@ fi
 
 if $WITH_CUDA ; then
   # install repo packages
-  CUDA_REPO_PKG=cuda-repo-ubuntu1404_7.5-18_amd64.deb
+  CUDA_REPO_PKG=cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
   wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG
   dpkg -i $CUDA_REPO_PKG
   rm $CUDA_REPO_PKG
