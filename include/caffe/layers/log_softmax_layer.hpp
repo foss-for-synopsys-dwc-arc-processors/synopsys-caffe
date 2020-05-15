1e060fa3ec093b65d6bec1e2c39d959f0ca825d4
Yvonne Chen <yche@synopsys.com>
2020-05-15 12:01:23 +0200

    add logsoftmax layer

---
+++ include/caffe/layers/log_softmax_layer.hpp
@@ -0,0 +1,46 @@
+#ifndef CAFFE_LOG_SOFTMAX_LAYER_HPP_
+#define CAFFE_LOG_SOFTMAX_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Computes the log of softmax function.
+ */
+template <typename Dtype>
+class LogSoftmaxLayer : public Layer<Dtype> {
+ public:
+  explicit LogSoftmaxLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "LogSoftmax"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom){
+    NOT_IMPLEMENTED;
+  }
+
+  int outer_num_;
+  int inner_num_;
+  int softmax_axis_;
+  /// sum_multiplier is used to carry out sum using BLAS
+  Blob<Dtype> sum_multiplier_;
+  /// scale is an intermediate Blob to hold temporary results.
+  Blob<Dtype> scale_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_LOG_SOFTMAX_LAYER_HPP_
