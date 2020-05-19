a0cf8a68fe094653ad196d77087c3383668216ed
Haifeng Han <haifenghan@qq.com>
2020-05-20 00:10:19 +0800

    MaskrcnnDetection -> MaskRCNNDetection; reformat the code

--- include/caffe/layers/maskrcnn_detection_layer.hpp
+++ include/caffe/layers/maskrcnn_detection_layer.hpp
@@ -7,44 +7,46 @@
 #include "caffe/layer.hpp"
 #include "caffe/proto/caffe.pb.h"
 
-
 namespace caffe {
 
-// implement of https://github.com/matterport/Mask_RCNN/blob/3deaec5d902d16e1daf56b62d5971d428dc920bc/mrcnn/model.py#L782
+// implement of
+// https://github.com/matterport/Mask_RCNN/blob/3deaec5d902d16e1daf56b62d5971d428dc920bc/mrcnn/model.py#L782
 
-template <typename Dtype>
-class MaskrcnnDetectionLayer : public Layer<Dtype> {
- public:
-  explicit MaskrcnnDetectionLayer(const LayerParameter& param)
+template <typename Dtype> class MaskRCNNDetectionLayer : public Layer<Dtype> {
+public:
+  explicit MaskRCNNDetectionLayer(const LayerParameter &param)
       : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+  virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
+                          const vector<Blob<Dtype> *> &top);
+  virtual void Reshape(const vector<Blob<Dtype> *> &bottom,
+                       const vector<Blob<Dtype> *> &top);
 
-  virtual inline const char* type() const { return "MaskrcnnDetection"; }
+  virtual inline const char *type() const { return "MaskRCNNDetection"; }
   virtual inline int ExactNumBottomBlobs() const { return 4; }
   virtual inline int ExactNumTopBlobs() const { return 1; }
 
- protected:
+protected:
+  inline vector<int> tf_nms(vector<vector<Dtype>> &pred_boxes,
+                            vector<int> &order, int topk,
+                            float iou_threshold) const;
 
-  inline vector<int> tf_nms(vector<vector<Dtype> > &pred_boxes, vector<int> &order, int topk, float iou_threshold) const;
-
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
+                           const vector<Blob<Dtype> *> &top);
+  // virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   //    const vector<Blob<Dtype>*>& top);
   /// @brief Not implemented
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  virtual void Backward_cpu(const vector<Blob<Dtype> *> &top,
+                            const vector<bool> &propagate_down,
+                            const vector<Blob<Dtype> *> &bottom) {
     NOT_IMPLEMENTED;
   }
-  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
+  //    {
   //  NOT_IMPLEMENTED;
   //}
 
-  //maskrcnn_detection params
+  // maskrcnn_detection params
   int batch_size_;
   int detection_max_instances_;
   float detection_min_confidence_;
@@ -53,10 +55,8 @@ class MaskrcnnDetectionLayer : public Layer<Dtype> {
 
   int N_;
   int num_class_;
-
 };
 
-}  // namespace caffe
+} // namespace caffe
 
-#endif  // CAFFE_MASKRCNN_DETECTION_LAYER_HPP_
-
+#endif // CAFFE_MASKRCNN_DETECTION_LAYER_HPP_
