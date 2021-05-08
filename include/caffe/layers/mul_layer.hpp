bdd65ad44e082cd85a1b1e38398b08b37e8102e0
Lucia Li <minglu@synopsys.com>
2021-05-08 09:01:19 +0200

    remove parameter of Mul

--- include/caffe/layers/mul_layer.hpp
+++ include/caffe/layers/mul_layer.hpp
@@ -10,39 +10,44 @@
 namespace caffe {
 
 /*
- * @brief Compute elementwise mul. Support broadcasting as numpy and TensorFlow do.
+ * @brief Compute elementwise mul. Support broadcasting as numpy and TensorFlow
+ * do.
  *
- * Note: Two dimensions are compatible for broadcasting if both are the same or either is 1.
- * The rule starts with the right-most dimension, and works towards the left-most dimension.
+ * Note: Two dimensions are compatible for broadcasting if both are the same or
+ * either is 1.
+ * The rule starts with the right-most dimension, and works towards the
+ * left-most dimension.
  */
 
-template <typename Dtype>
-class MulLayer : public Layer<Dtype> {
- public:
-  explicit MulLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+template <typename Dtype> class MulLayer : public Layer<Dtype> {
+public:
+  explicit MulLayer(const LayerParameter &param) : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
+                          const vector<Blob<Dtype> *> &top);
+  virtual void Reshape(const vector<Blob<Dtype> *> &bottom,
+                       const vector<Blob<Dtype> *> &top);
 
-  virtual inline const char* type() const { return "Mul"; }
+  virtual inline const char *type() const { return "Mul"; }
   virtual inline int MinBottomBlobs() const { return 1; }
   virtual inline int ExactNumTopBlobs() const { return 1; }
 
- protected:
+protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
+                           const vector<Blob<Dtype> *> &top);
 
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom){
+  virtual void Backward_cpu(const vector<Blob<Dtype> *> &top,
+                            const vector<bool> &propagate_down,
+                            const vector<Blob<Dtype> *> &bottom) {
     NOT_IMPLEMENTED;
   };
 
   int dim_diff_;
   int dim_;
   bool is_scalar_;
+  Blob<Dtype> *multiplier;
+  vector<int> mul_shape;
 };
 
-}  // namespace caffe
+} // namespace caffe
 
-#endif  // CAFFE_MUL_LAYER_HPP_
+#endif // CAFFE_MUL_LAYER_HPP_
