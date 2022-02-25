98c49a590709666064b079ae36459f34b96cc892
lyou <justin.you@synopsys.com>
2022-02-25 10:02:00 +0800

    Update FindSnappy.cmake

--- cmake/Modules/FindSnappy.cmake
+++ cmake/Modules/FindSnappy.cmake
@@ -5,27 +5,35 @@
 #
 # The following are set after configuration is done:
 #  SNAPPY_FOUND
-#  Snappy_INCLUDE_DIR
+#  Snappy_INCLUDE_DIRS
 #  Snappy_LIBRARIES
-if(MSVC)
-  # rely on snappy-config.cmake
-  find_package(Snappy NO_MODULE)
-else()
-  find_path(Snappy_INCLUDE_DIR NAMES snappy.h
-                              PATHS ${SNAPPY_ROOT_DIR} ${SNAPPY_ROOT_DIR}/include)
 
-  find_library(Snappy_LIBRARIES NAMES snappy
-                                PATHS ${SNAPPY_ROOT_DIR} ${SNAPPY_ROOT_DIR}/lib)
-endif()
+find_path(Snappy_INCLUDE_DIRS
+  NAMES snappy.h
+  HINTS ${snappy_ROOT_DIR}/include)
+
+find_library(Snappy_LIBRARIES
+  NAMES snappy
+  HINTS ${snappy_ROOT_DIR}/lib)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Snappy DEFAULT_MSG Snappy_INCLUDE_DIR Snappy_LIBRARIES)
+find_package_handle_standard_args(Snappy DEFAULT_MSG Snappy_LIBRARIES Snappy_INCLUDE_DIRS)
 
-if(SNAPPY_FOUND)
-  message(STATUS "Found Snappy  (include: ${Snappy_INCLUDE_DIR}, library: ${Snappy_LIBRARIES})")
-  mark_as_advanced(Snappy_INCLUDE_DIR Snappy_LIBRARIES)
+mark_as_advanced(
+  Snappy_LIBRARIES
+  Snappy_INCLUDE_DIRS)
 
-  caffe_parse_header(${Snappy_INCLUDE_DIR}/snappy-stubs-public.h
+if(SNAPPY_FOUND AND NOT (TARGET Snappy::snappy))
+
+  add_library (Snappy::snappy UNKNOWN IMPORTED)
+  set_target_properties(Snappy::snappy
+    PROPERTIES
+      IMPORTED_LOCATION ${Snappy_LIBRARIES}
+      INTERFACE_INCLUDE_DIRECTORIES ${Snappy_INCLUDE_DIRS})
+      
+  message(STATUS "Found Snappy  (include: ${Snappy_INCLUDE_DIRS}, library: ${Snappy_LIBRARIES})")
+
+  caffe_parse_header(${Snappy_INCLUDE_DIRS}/snappy-stubs-public.h
                      SNAPPY_VERION_LINES SNAPPY_MAJOR SNAPPY_MINOR SNAPPY_PATCHLEVEL)
   set(Snappy_VERSION "${SNAPPY_MAJOR}.${SNAPPY_MINOR}.${SNAPPY_PATCHLEVEL}")
 endif()
