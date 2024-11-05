166fdb811ec4adebe476d95d573ee85a32245052
Minglu Li <minglu@synopsys.com>
2024-11-05 07:47:00 +0100

    refine cmake file to find LMDB

--- cmake/Modules/FindLMDB.cmake
+++ cmake/Modules/FindLMDB.cmake
@@ -14,6 +14,8 @@
 
 if(MSVC)
   find_package(LMDB NO_MODULE)
+  find_path(LMDB_INCLUDE_DIR NAMES  lmdb.h PATHS "$ENV{LMDB_DIR}/include")
+  find_library(LMDB_LIBRARIES NAMES lmdb   PATHS "$ENV{LMDB_DIR}/lib" )
 else()
   find_path(LMDB_INCLUDE_DIR NAMES  lmdb.h PATHS "$ENV{LMDB_DIR}/include")
   find_library(LMDB_LIBRARIES NAMES lmdb   PATHS "$ENV{LMDB_DIR}/lib" )
